9cdf0d716ef113f15af22448838f82808d2909b7
ABHISHEK MAURYA <118133017+abhishekmaurya000786@users.noreply.github.com>
2025-01-28 10:33:56 +0000

    final changes made

--- project/app.py
+++ project/app.py
@@ -1,3 +1,18 @@
-# TODO Build the app.py file along with the template.html file to create a simple To-Do List app
+#  Build the app.py file along with the template.html file to create a simple To-Do List app
 # The app should have routes for adding a task, and should show a list of tasks
 # Everything you have learned above will be useful for this task
+
+from flask import Flask, render_template, request, redirect, url_for
+app = Flask(__name__)
+
+tasks = []
+@app.route('/')
+def home():
+    return render_template('home.html', tasks=tasks)
+@app.route('/add', methods=['POST'])
+def add_task():
+    task = request.form['task']
+    tasks.append(task)
+        return redirect(url_for('home'))
+if __name__ == '__main__':
+app.run(debug=True)
